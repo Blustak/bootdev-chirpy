d473003f207e05098c9805057869bc0c99981b5d
Carl Menditta <menditta1@gmail.com>
2025-09-26 17:29:35 +0100

    add: app,readiness,logo endpoints

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import (
     "net/http"
 )
 
+
 func main() {
     serve := http.NewServeMux()
+    serve.HandleFunc("/healthz",readinessHandler)
+    fileServeHandle := http.StripPrefix("/app",http.FileServer(http.Dir(".")))
+    serve.Handle("/app/",fileServeHandle)
+    serve.Handle("/assets", http.FileServer(http.Dir("./assets")))
     server := http.Server{
         Handler: serve,
         Addr: ":8080",
@@ -13,3 +18,10 @@ func main() {
 
     server.ListenAndServe()
 }
+
+func readinessHandler(resW http.ResponseWriter,req *http.Request) {
+    resW.Header().Add("Content-Type","text/plain; charset=utf-8")
+    resW.WriteHeader(200)
+    resW.Write([]byte("OK"))
+}
+
